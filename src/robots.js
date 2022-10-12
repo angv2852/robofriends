388f234dd6dbc5e469a23322bf6f7ff1d2b608b6
angv2852 <angv2852@gmail.com>
2022-10-12 15:23:26 -0600

    Created robofriends and created cards

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Mr. Roboto",
+    username: "Robo",
+    email: "roboto-man@metalmania.com",
+  },
+  {
+    id: 2,
+    name: "Mr. Roboto",
+    username: "Robo",
+    email: "roboto-man@metalmania.com",
+  },
+  {
+    id: 3,
+    name: "Mr. Roboto",
+    username: "Robo",
+    email: "roboto-man@metalmania.com",
+  },
+  {
+    id: 4,
+    name: "Mr. Roboto",
+    username: "Robo",
+    email: "roboto-man@metalmania.com",
+  },
+  {
+    id: 5,
+    name: "Mr. Roboto",
+    username: "Robo",
+    email: "roboto-man@metalmania.com",
+  },
+  {
+    id: 6,
+    name: "Mr. Roboto",
+    username: "Robo",
+    email: "roboto-man@metalmania.com",
+  },
+  {
+    id: 7,
+    name: "Mr. Roboto",
+    username: "Robo",
+    email: "roboto-man@metalmania.com",
+  },
+  {
+    id: 8,
+    name: "Mr. Roboto",
+    username: "Robo",
+    email: "roboto-man@metalmania.com",
+  },
+  {
+    id: 9,
+    name: "Mr. Roboto",
+    username: "Robo",
+    email: "roboto-man@metalmania.com",
+  },
+  {
+    id: 10,
+    name: "Mr. Roboto",
+    username: "Robo",
+    email: "roboto-man@metalmania.com",
+  },
+];
